fbe311b2bb0cb8f322a9648f6fa58c653fff854c
ajmandourah <ajmandourah@gmail.com>
2024-06-06 17:56:26 +0300

    editing themes

--- package/batocera/emulationstation/es-theme-minima/es-theme-minima.mk
+++ package/batocera/emulationstation/es-theme-minima/es-theme-minima.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 # Version.: Commits on Feb 13, 2024
-ES_THEME_MINIMA_VERSION =
-ES_THEME_MINIMA_SITE = https://github.com/soaremicheledavid/ES-Theme-MINIMA/archive/refs/heads/master.tar.gz
+ES_THEME_MINIMA_VERSION = b8eebd0a80fab84e61674239d2236051dc3c1ce3
+ES_THEME_MINIMA_SITE = $(call github,soaremicheledavid,es-theme-minima,$(ES_THEME_MINIMA_VERSION))
 
 define ES_THEME_MINIMA_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-minima
