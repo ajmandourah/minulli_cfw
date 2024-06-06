5e6dde11d102258eae4583947496d08655ca3f04
ajmandourah <ajmandourah@gmail.com>
2024-06-06 08:37:11 +0300

    change to rgui

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -5,7 +5,7 @@
 ################################################################################
 # Version.: Commits on Feb 13, 2024
 ES_THEME_CARBON_VERSION = 6b868454cad63296a3f36b1d3c5307d60f23ac2b
-ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
+ES_THEME_CARBON_SITE = $(call github,soaremicheledavid,es-theme-minima)
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon
