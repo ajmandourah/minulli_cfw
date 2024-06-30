e94c881dcbd599805606a2660888bdcd4945a407
ajmandourah <ajmandourah@gmail.com>
2024-06-30 21:32:45 +0300

    Revert "test"
    This reverts commit 88e4fa8d00d13f89eb8eec7b5dd5df0384e4377d.

--- board/batocera/allwinner/h700/rg28xx/fsoverlay/etc/init.d/S99z-retroarch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Check if retroarch is running meaning that a game got resumed
-if pidof retroarch >/dev/null; then
-    while pidof retroarch >/dev/null; do sleep 0.1; done
-    emulationstation-standalone &
-fi
-
